d900d819aa6a090518a10510da7ca43fda830d65
mitul <mitulms9@gmail.com>
2026-08-14 01:25:58 +0530

    Use Render API URL

--- admin/js/auth.js
+++ admin/js/auth.js
@@ -1,4 +1,6 @@
-const API = "http://127.0.0.1:8000";
+//const API = "http://127.0.0.1:8000";
+const API = "https://slotly-usly.onrender.com";
+
 
 
 // ==========================================================
